c3ddd0511b2e493060a62d4422148b3f0da1591e
Tomasz Habiger <tomasz.habiger@gmail.com>
2016-12-23 12:57:14 +0100

    Adds blob copy function

---
+++ THAzure-CopyBlob.ps1
@@ -0,0 +1,23 @@
+﻿function Copy-AzureBlob{
+    [CmdletBinding(
+        SupportsShouldProcess=$True
+    )]
+    param (
+        [Parameter(Mandatory = $true)]   
+        [String] $resourceGroupName,
+        [Parameter(Mandatory = $true)]   
+        [String] $storageAccountName,
+        [Parameter(Mandatory = $true)]   
+        [String] $containerName,
+        [Parameter(Mandatory = $true)]   
+        [String] $filePath
+    )
+
+    $storageAccountKey = (Get-AzureRmStorageAccountKey -StorageAccountName $storageAccountName -ResourceGroupName $resourceGroupName)[0].Value
+    $blobContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+
+    $file = Get-Item $filePath
+    
+    Write-Verbose "Copying $fileName to $blobName"
+    Set-AzureStorageBlobContent -File $filePath -Container $containerName -Blob $file.Name -Context $blobContext -Force
+}
